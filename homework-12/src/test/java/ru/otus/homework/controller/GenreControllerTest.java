95fa2c2d7dcedecad5877d95163968b0112881a4
Alexey Khudyakov <khudyakov.alexey@gmail.com>
2020-03-09 23:19:52 +0300

    Домашнее задание #12
    В CRUD Web-приложение добавить механизм аутентификации
    Цель: Цель: защитить Web-приложение аутентифкацией и простой авторизацией Результат: приложение с использованием Spring Security
    Внимание! Задание выполняется на основе нереактивного приложения Sping MVC!
    
    1. Добавить в приложение новую сущность - пользователь. Не обязательно реализовывать методы по созданию пользователей - допустимо добавить пользователей только через БД-скрипты.
    2. В существующее CRUD-приложение добавить механизм Form-based аутентификации.
    3. UsersServices реализовать самостоятельно.
    4. Авторизация на всех страницах - для всех аутентифицированных. Форма логина - доступна для всех.

--- homework-12/src/test/java/ru/otus/homework/controller/GenreControllerTest.java
+++ homework-12/src/test/java/ru/otus/homework/controller/GenreControllerTest.java
@@ -4,10 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.otus.homework.service.GenreService;
-import ru.otus.homework.service.UserDetailsServiceImpl;
 import ru.otus.homework.service.UserService;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -19,7 +19,7 @@ public class GenreControllerTest {
     @Autowired
     private MockMvc mvc;
     @MockBean
-    private UserDetailsServiceImpl userDetailsService;
+    private UserDetailsService userDetailsService;
     @MockBean
     private UserService userService;
     @MockBean
